e9285ea2d2793d76f5df719ad913030a076cf405
Dustin Spicuzza <dustin@virtualroadside.com>
2019-10-12 18:19:08 -0400

    Use autogenerated module initializer instead

--- subprojects/pyntcore/ntcore/_impl/ntcore.cpp
+++ subprojects/pyntcore/ntcore/_impl/ntcore.cpp
@@ -1,19 +1,7 @@
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-
-namespace py = pybind11;
-
-// forward declarations
-void init_NetworkTable(py::module &m);
-void init_NetworkTableEntry(py::module &m);
-void init_NetworkTableInstance(py::module &m);
-void init_NetworkTableType(py::module &m);
+#include "gensrc/module.hpp"
 
 PYBIND11_MODULE(ntcore, m)
 {
-  init_NetworkTable(m);
-  init_NetworkTableEntry(m);
-  init_NetworkTableInstance(m);
-  init_NetworkTableType(m);
+  initWrapper(m);
 }
