2de2cd52eab2aa7954ea93ef8171076efbf04876
Tim Winters <twinters@wpi.edu>
2020-01-01 11:57:31 -0500

    Add ArrayRef type_cast

--- subprojects/robotpy-wpiutil/wpiutil/include/wpiutil_converters.hpp
+++ subprojects/robotpy-wpiutil/wpiutil/include/wpiutil_converters.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <wpi/StringRef.h>
 #include <wpi/Twine.h>
@@ -75,6 +76,43 @@ public:
     static handle cast(const wpi::Twine &t, return_value_policy, handle)
     {
         throw py::cast_error("wpi::Twine should never be a return value");
+    }
+};
+
+template <typename Type> struct type_caster<wpi::ArrayRef<Type>> {
+    using value_conv = make_caster<Type>;
+    PYBIND11_TYPE_CASTER(wpi::ArrayRef<Type>, _("List[") + value_conv::name + _("]"));
+
+    std::vector<Type> vec;
+    bool load(handle src, bool convert) {
+        if (!isinstance<sequence>(src) || isinstance<str>(src))
+            return false;
+        auto s = reinterpret_borrow<sequence>(src);
+        vec.reserve(s.size());
+        for (auto it : s) {
+            value_conv conv;
+            if (!conv.load(it, convert))
+                return false;
+            vec.push_back(cast_op<Type &&>(std::move(conv)));
+        }
+        value = wpi::ArrayRef<Type>(vec);
+        return true;
+    }
+
+public:
+    template <typename T>
+    static handle cast(T &&src, return_value_policy policy, handle parent) {
+        if (!std::is_lvalue_reference<T>::value)
+            policy = return_value_policy_override<Type>::policy(policy);
+        list l(src.size());
+        size_t index = 0;
+        for (auto &&value : src) {
+            auto value_ = reinterpret_steal<object>(value_conv::cast(forward_like<T>(value), policy, parent));
+            if (!value_)
+                return handle();
+            PyList_SET_ITEM(l.ptr(), (ssize_t) index++, value_.release().ptr()); // steals a reference
+        }
+        return l.release();
     }
 };
 
