af4fba9a694433031c01a8f2a2653c8796aa71bf
Dustin Spicuzza <dustin@virtualroadside.com>
2020-01-11 22:34:54 -0500

    Formatting

--- subprojects/robotpy-wpiutil/tests/test_smallset.py
+++ subprojects/robotpy-wpiutil/tests/test_smallset.py
@@ -1,11 +1,17 @@
-
 from wpiutil_test import module
+
 
 def test_smallset_load():
     assert module.load_smallset_int({1, 2, 3, 4}) == {1, 2, 3, 4}
-    
+
+
 def test_smallsetbool_load():
-    assert module.load_smallset_int({True, True, False, True}) == {True, True, False, True}
+    assert module.load_smallset_int({True, True, False, True}) == {
+        True,
+        True,
+        False,
+        True,
+    }
 
 
 def test_smallset_cast():
--- subprojects/robotpy-wpiutil/tests/test_stringref.py
+++ subprojects/robotpy-wpiutil/tests/test_stringref.py
@@ -1,20 +1,25 @@
-
 from wpiutil_test import module
+
 
 def test_stringref_load():
     assert module.load_stringref("something") == "something"
 
+
 def test_stringref_cast():
     assert module.cast_stringref() == "casted"
+
 
 def test_arrayref_load_int():
     assert module.load_arrayref_int([1, 2, 3]) == [1, 2, 3]
 
+
 def test_arrayref_load_bool():
     assert module.load_arrayref_bool([True, False, True]) == [True, False, True]
 
+
 def test_arrayref_load_string():
-    assert module.load_arrayref_string(['a', 'b', 'c']) == ['a', 'b', 'c']
+    assert module.load_arrayref_string(["a", "b", "c"]) == ["a", "b", "c"]
+
 
 def test_arrayref_load_vector():
-    assert module.load_arrayref_vector([['a'], ['b'], ['c']]) == [['a'],['b'],['c']]
+    assert module.load_arrayref_vector([["a"], ["b"], ["c"]]) == [["a"], ["b"], ["c"]]
