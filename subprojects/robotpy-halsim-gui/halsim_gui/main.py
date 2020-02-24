a7a54fc3c39c0ec7c5ed6eef0936f2e36b1343e6
Dustin Spicuzza <dustin@virtualroadside.com>
2020-02-23 23:10:07 -0500

    Move simulation entrypoint to pyfrc instead

--- subprojects/robotpy-halsim-gui/halsim_gui/main.py
+++ subprojects/robotpy-halsim-gui/halsim_gui/main.py
@@ -5,30 +5,19 @@ from os.path import abspath, dirname, join
 logger = logging.getLogger("halsim_gui")
 
 
-class HalSimGuiMain:
-    """
-        Runs the robot using WPILib's GUI HAL Simulator
-    """
+def loadExtension():
+    try:
+        import hal
+    except ImportError as e:
+        # really, should never happen...
+        raise ImportError("you must install robotpy-hal!") from e
 
-    def __init__(self, parser):
-        pass
+    from .version import version
 
-    def run(self, options, robot_class, **static_options):
+    logger.info("WPILib HAL Simulation %s", version)
 
-        try:
-            import hal
-        except ImportError:
-            # really, should never happen...
-            raise ImportError("you must install robotpy-hal!")
-
-        from .version import version
-
-        logger.info("WPILib HAL Simulation %s", version)
-
-        root = join(abspath(dirname(__file__)), "lib")
-        ext = join(root, os.listdir(root)[0])
-        retval = hal.loadOneExtension(ext)
-        if retval != 0:
-            logger.warn("loading extension may have failed (error=%d)", retval)
-
-        return robot_class.main(robot_class)
+    root = join(abspath(dirname(__file__)), "lib")
+    ext = join(root, os.listdir(root)[0])
+    retval = hal.loadOneExtension(ext)
+    if retval != 0:
+        logger.warn("loading extension may have failed (error=%d)", retval)
