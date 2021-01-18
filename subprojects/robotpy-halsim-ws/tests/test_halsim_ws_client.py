f3fb5760a222c3cf5162533acf961c0a4cc4185c
TheTripleV <vasistavovveti@gmail.com>
2021-01-18 04:03:02 -0500

    initial commit

---
+++ subprojects/robotpy-halsim-ws/tests/test_halsim_ws_client.py
@@ -0,0 +1,6 @@
+# tests the internals
+import halsim_ws_client._init_halsim_ws_client
+
+
+def test_halsim_ws_client():
+    pass
---
+++ subprojects/robotpy-halsim-ws/tests/test_halsim_ws_server.py
@@ -0,0 +1,6 @@
+# tests the internals
+import halsim_ws_server._init_halsim_ws_server
+
+
+def test_halsim_ws_server():
+    pass
