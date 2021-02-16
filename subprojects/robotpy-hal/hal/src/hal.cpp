74762be7a23e6e60ea15ac01c3f1cc14028c502a
Dustin Spicuzza <dustin@virtualroadside.com>
2021-02-16 00:00:15 -0500

    Formatting

--- subprojects/robotpy-hal/hal/src/hal.cpp
+++ subprojects/robotpy-hal/hal/src/hal.cpp
@@ -3,13 +3,13 @@
 #include <rpygen_wrapper.hpp>
 
 RPYBUILD_PYBIND11_MODULE(m) {
-    initWrapper(m);
+  initWrapper(m);
 
 #ifdef __FRC_ROBORIO__
-    m.attr("__halplatform__") = "roboRIO";
-    m.attr("__hal_simulation__") = false;
+  m.attr("__halplatform__") = "roboRIO";
+  m.attr("__hal_simulation__") = false;
 #else
-    m.attr("__halplatform__") = "sim";
-    m.attr("__hal_simulation__") = true;
+  m.attr("__halplatform__") = "sim";
+  m.attr("__hal_simulation__") = true;
 #endif
 }
