1a966b306e1d30c1e595bd4992816c784580c223
Dustin Spicuzza <dustin@virtualroadside.com>
2022-03-02 02:19:18 -0500

    Add specializations to span caster for uint8_t
    - Typical use is going to be some kind of memory buffer, so we allow anything
      that implements the buffer protocol to work

--- subprojects/robotpy-wpiutil/tests/test_span.py
+++ subprojects/robotpy-wpiutil/tests/test_span.py
@@ -1,4 +1,6 @@
+import pytest
 from wpiutil_test import module
+import array
 
 
 def test_span_load_int():
@@ -27,6 +29,35 @@ def test_span_load_stringview():
 
 def test_span_load_vector():
     assert module.load_span_vector([["a"], ["b"], ["c"]]) == [["a"], ["b"], ["c"]]
+
+
+def test_span_load_buffer_bytes():
+    assert module.load_span_bytes(b"abc") == b"abc"
+
+
+def test_span_modify_buffer_bytes():
+    b = b"abc"
+    with pytest.raises(BufferError):
+        module.modify_span_buffer(b)
+
+
+def test_span_load_buffer_bytearray():
+    assert module.load_span_bytes(bytearray([1, 2, 3])) == b"\x01\x02\x03"
+
+
+def test_span_modify_buffer_bytearray():
+    b = bytearray([1, 2, 3])
+    module.modify_span_buffer(b)
+    assert b == bytearray([4, 2, 3])
+
+
+def test_span_load_buffer_array():
+    a = array.array("l")
+    a.append(1)
+    a2 = array.array("l")
+    a2.frombytes(module.load_span_bytes(a))
+    assert len(a2) == 1
+    assert a2[0] == 1
 
 
 def test_span_cast():
