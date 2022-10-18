d70219140d84f07aff64a710d86031caefc53e72
Dustin Spicuzza <dustin@virtualroadside.com>
2022-10-18 01:33:05 -0400

    Add synchronization primitives for wpilib interop

---
+++ subprojects/robotpy-wpiutil/wpiutil/sync/__init__.py
@@ -0,0 +1,31 @@
+from .._wpiutil.sync import (
+    createEvent,
+    createSemaphore,
+    createSignalObject,
+    destroyEvent,
+    destroySemaphore,
+    destroySignalObject,
+    releaseSemaphore,
+    resetEvent,
+    resetSignalObject,
+    setEvent,
+    setSignalObject,
+    waitForObject,
+    waitForObjects,
+)
+
+__all__ = [
+    "createEvent",
+    "createSemaphore",
+    "createSignalObject",
+    "destroyEvent",
+    "destroySemaphore",
+    "destroySignalObject",
+    "releaseSemaphore",
+    "resetEvent",
+    "resetSignalObject",
+    "setEvent",
+    "setSignalObject",
+    "waitForObject",
+    "waitForObjects",
+]
