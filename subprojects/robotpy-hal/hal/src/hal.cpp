0500b1ebaa1fcab493baaedb9b5b09b57d50f4e9
Dustin Spicuzza <dustin@virtualroadside.com>
2022-12-10 01:29:34 -0500

    Use HAL_SetPrintErrorImpl to send error messages to python's stderr
    - This should ensure error messages don't get weirdly interleaved
    - Fixes https://github.com/robotpy/robotpy-wpilib/issues/632

--- subprojects/robotpy-hal/hal/src/hal.cpp
+++ subprojects/robotpy-hal/hal/src/hal.cpp
@@ -1,8 +1,13 @@
 
 
 #include <hal/HALBase.h>
+#include <hal/DriverStation.h>
 #include <hal/Value.h>
 #include <rpygen_wrapper.hpp>
+
+using namespace pybind11::literals;
+
+static py::module_ sys_module;
 
 RPYBUILD_PYBIND11_MODULE(m) {
   initWrapper(m);
@@ -24,9 +29,28 @@ RPYBUILD_PYBIND11_MODULE(m) {
     .value("INT", HAL_Type::HAL_INT)
     .value("LONG", HAL_Type::HAL_LONG);
 
+  // Redirect stderr to python stderr
+  sys_module = py::module_::import("sys");
 
-  // Call HAL_Shutdown on module unload
+  HAL_SetPrintErrorImpl([](const char *line, size_t size) {
+    py::gil_scoped_acquire lock;
+    py::print(py::str(line, size), "file"_a=sys_module.attr("stderr"));
+  });
+
+  // Do cleanup on module unload
   static int unused; // the capsule needs something to reference
-  py::capsule cleanup(&unused, [](void *) { HAL_Shutdown(); });
+  py::capsule cleanup(&unused, [](void *) {
+    {
+      py::gil_scoped_acquire lock;
+      HAL_SetPrintErrorImpl(nullptr);
+      sys_module.dec_ref();
+      sys_module.release();
+    }
+
+    {
+      py::gil_scoped_release unlock;
+      HAL_Shutdown();
+    }
+  });
   m.add_object("_cleanup", cleanup);
 }
