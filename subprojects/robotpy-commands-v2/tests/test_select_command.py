d93424b20608b6c81689e34f1a364bb38e0c9a32
Dustin Spicuzza <dustin@virtualroadside.com>
2022-12-18 23:57:36 -0500

    Add SelectCommand implementation
    - Fixes #14

---
+++ subprojects/robotpy-commands-v2/tests/test_select_command.py
@@ -0,0 +1,20 @@
+import commands2
+from util import ConditionHolder
+
+
+def test_select_command(scheduler: commands2.CommandScheduler):
+    c = ConditionHolder()
+
+    def _assert_false():
+        assert False
+
+    cmd1 = commands2.RunCommand(_assert_false)
+    cmd2 = commands2.RunCommand(c.setTrue)
+    cmd3 = commands2.RunCommand(_assert_false)
+
+    sc = commands2.SelectCommand(lambda: 2, [(1, cmd1), (2, cmd2), (3, cmd3)])
+
+    scheduler.schedule(sc)
+    scheduler.run()
+
+    assert c.cond
