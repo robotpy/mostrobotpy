98575bb2bb71d7b3055795a1f42f080101e4df6c
Dustin Spicuzza <dustin@virtualroadside.com>
2023-12-12 03:41:05 -0500

    Fix documentation quirks
    - Enable __init__ documentation
    - Fix ping-rtd github action

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
+autoclass_content = "both"
+
 
 # -- Custom Document processing ----------------------------------------------
 
