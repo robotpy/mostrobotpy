c39cdb85f91b9b0161de163debd864bdc92340a0
Dustin Spicuzza <dustin@virtualroadside.com>
2025-04-19 23:51:53 -0400

    Update robotpy-halsim-ds-socket to use hatch-robotpy

--- subprojects/robotpy-halsim-ds-socket/tests/test_halsim_ds_socket.py
+++ subprojects/robotpy-halsim-ds-socket/tests/test_halsim_ds_socket.py
@@ -1,6 +1,18 @@
-# tests the internals
-import halsim_ds_socket._init_halsim_ds_socket
+import ctypes
+import pathlib
 
 
 def test_halsim_ds_socket():
-    pass
+    # dependencies
+    import native.wpihal._init_robotpy_native_wpihal
+    import native.wpinet._init_robotpy_native_wpinet
+
+    import halsim_ds_socket as base
+
+    loaded = 0
+    for fname in (pathlib.Path(base.__file__).parent / "lib").iterdir():
+        if fname.is_file() and fname.suffix in (".dll", ".dylib", ".so"):
+            ctypes.CDLL(str(fname))
+            loaded += 1
+
+    assert loaded
