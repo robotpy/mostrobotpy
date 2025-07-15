e52c8eae51ca60d3379c70986750090fa67aa472
David Vo <auscompgeek@users.noreply.github.com>
2025-07-16 01:17:44 +1000

    Add test for TrapezoidProfile.State repr

--- subprojects/robotpy-wpimath/tests/test_trapezoid_profile.py
+++ subprojects/robotpy-wpimath/tests/test_trapezoid_profile.py
@@ -14,3 +14,11 @@ def test_constraints_repr(TrapezoidProfile):
     constraints = TrapezoidProfile.Constraints()
 
     assert repr(constraints).startswith(f"{expected_qualname}(maxVelocity=0.")
+
+
+@pytest.mark.parametrize("TrapezoidProfile", trapezoid_profile_types)
+def test_state_repr(TrapezoidProfile):
+    expected_qualname = f"{TrapezoidProfile.__name__}.State"
+    constraints = TrapezoidProfile.State()
+
+    assert repr(constraints).startswith(f"{expected_qualname}(position=0.")
