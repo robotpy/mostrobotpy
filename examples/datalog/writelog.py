189c603d0b1d430255a53481625e6ddd7f5a5981
Dustin Spicuzza <dustin@virtualroadside.com>
2026-08-08 10:29:19 +0000

    docs: demonstrate automatic wpilog struct decoding

--- examples/datalog/writelog.py
+++ examples/datalog/writelog.py
@@ -3,7 +3,9 @@
 import argparse
 import pathlib
 
-from wpilog import DataLog, BooleanLogEntry, StringArrayLogEntry, RawLogEntry
+import wpilog
+
+from datalog_struct import ExampleRecord
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -11,18 +13,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.out.is_dir():
-        datalog = DataLog(str(args.out))
+        datalog = wpilog.DataLogBackgroundWriter(str(args.out))
     else:
-        datalog = DataLog(str(args.out.parent), args.out.name)
+        datalog = wpilog.DataLogBackgroundWriter(str(args.out.parent), args.out.name)
 
-    bools = BooleanLogEntry(datalog, "/bools")
+    bools = wpilog.BooleanLogEntry(datalog, "/bools")
     bools.append(True)
     bools.append(False)
 
-    strings = StringArrayLogEntry(datalog, "/strings")
+    strings = wpilog.StringArrayLogEntry(datalog, "/strings")
     strings.append(["a", "b", "c"])
     strings.append(["d", "e", "f"])
 
-    raw = RawLogEntry(datalog, "/raws")
+    raw = wpilog.RawLogEntry(datalog, "/raws")
     raw.append(b"\x01\x02\x03")
     raw.append(b"\x04\x05\x06")
+
+    record = wpilog.StructLogEntry(datalog, "/record", ExampleRecord)
+    record.append(ExampleRecord(1, 2))
+    record.append(ExampleRecord(3, 4))
